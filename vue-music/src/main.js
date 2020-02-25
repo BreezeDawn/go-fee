97110eb7cefe368997fe9dbb62f39d25a88eefdf
maining <mmmynnn@outlook.com>
2020-02-26 07:47:07 +0800

    update main.js with import babel-polyfill/fastclick

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -1,8 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import fastclick from 'fastclick'
 import 'common/stylus/index.styl'
+
+// attach 到 document.body Dom 上
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
