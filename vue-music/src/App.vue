443185b1989f1003580764293cc4f2e688762f47
maining <mmmynnn@outlook.com>
2020-02-26 07:49:06 +0800

    update App.vue with import m-header

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    hello world
+    <m-header></m-header>
   </div>
 </template>
 
 <script type="text/ecmasript-6">
-
+import MHeader from 'components/m-header/m-header'
+export default {
+  components: {
+    MHeader
+  }
+}
 </script>
 
 <style lang="stylus" scoped rel="stylesheet/stylus">
-  @import "~common/stylus/variable";
-  #app
-    color: $color-theme
 </style>
